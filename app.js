f9b4dab88f8b83d06a7dd91e7f4cd7bd4d811011
Tuguldur Bayarnyam <tuguldur@Tuguldurs-MacBook-Pro.local>
2018-01-16 16:23:55 -0800

    Added & Changed index page

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
-// uncomment after placing your favicon in /public
+// uncomment after placing your favicon ssin /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
@@ -32,6 +32,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', index);
 app.use('/users', users);
 
+
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   var err = new Error('Not Found');
@@ -40,7 +41,7 @@ app.use(function(req, res, next) {
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function(err, req, res, next) { 
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,16 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/post/:id', function(req, res, next) {
+  res.render('post', { id: req.params.id });
+});
+
+router.get("/profile", function (req,res) {
+  res.render('profile')
+})
+
+router.get("/work", function (req,res) {
+  res.render('work')
+})
+
 module.exports = router;
