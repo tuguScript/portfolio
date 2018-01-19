3c955abfbec51df61a56fe785116757a645a50ed
Tuguldur Bayarnyam <tuguldur@Tuguldurs-MacBook-Pro.local>
2018-01-19 15:07:20 -0800

    styled post page

--- src/data.js
+++ src/data.js
@@ -1,38 +1,103 @@
 export const featuredArticles = [
     {
-        "id": 1,
-        "title": "Todoro.ga",
-        "category": "Progressive Web App",
-        "img": 'http://lorempixel.com/400/400'
+        'id': 1,
+        'date': '12/06/2018',
+        'title': 'Todoro.ga',
+        'category': 'Progressive Web App',
+        'img': 'https://images.unsplash.com/photo-1454778640289-e73d538b445d?auto=format&fit=crop&w=1950&q=80',
+        'content': `Laborum voluptate consequat exercitation non ipsum sit occaecat pariatur exercitation ea aute enim sit consequat. Sunt velit ea dolor sunt nulla enim consectetur eiusmod eu occaecat. Labore ullamco reprehenderit occaecat mollit voluptate ut enim cillum id qui culpa. Labore fugiat pariatur pariatur proident ut ullamco Lorem ullamco elit. Nisi commodo dolore nisi ullamco labore.
+
+Cillum id id eu occaecat ex amet proident et amet do aliqua pariatur adipisicing. Sint sint reprehenderit elit pariatur voluptate enim deserunt quis. Reprehenderit sit velit ullamco non mollit in exercitation excepteur. Labore laborum nostrud ullamco incididunt minim aliquip fugiat anim amet amet nulla duis. Commodo consectetur minim sunt ullamco dolore aliquip nostrud culpa laboris ipsum aliqua occaecat sunt. Ullamco ex nostrud laboris ad qui ut. Consequat laboris sint sunt et laborum minim non ea esse dolore dolor ut incididunt duis.
+
+Occaecat duis quis incididunt amet est cupidatat eu tempor laborum reprehenderit quis esse sit. Proident tempor sint ea cillum. Consectetur ex dolor ullamco mollit eiusmod irure deserunt dolor. Aliqua sunt pariatur non magna voluptate exercitation elit sunt non eu labore. Nulla occaecat eiusmod irure Lorem quis Lorem amet cillum cillum labore et duis dolor.
+
+Culpa in elit veniam fugiat duis aliqua. Minim fugiat id pariatur dolor officia sint adipisicing eiusmod non. Id deserunt aute adipisicing nostrud dolore. Do nostrud minim consequat dolore tempor laboris fugiat Lorem mollit sint culpa amet anim elit. Incididunt cillum ut consectetur dolore ex eu.
+
+Eiusmod laborum et consequat enim voluptate fugiat cillum enim irure amet irure. Cupidatat cillum consectetur laborum duis ex commodo. Fugiat ea fugiat deserunt exercitation quis culpa non culpa do voluptate. Veniam dolor in nostrud ad consequat elit id deserunt incididunt ullamco aute minim labore ad. Ex fugiat tempor Lorem ullamco est proident cillum irure.`
     },
     {
-        "id": 2,
-        "title": "Benefits of Cold Shower",
-        "category": "Progressive Web App",
-        "img": 'http://lorempixel.com/400/400'
+        'id': 2,
+        'date': '12/06/2018',
+        'title': 'Benefits of Cold Shower',
+        'category': 'Progressive Web App',
+        'img': 'https://images.unsplash.com/reserve/unsplash_528b27288f41f_1.JPG?auto=format&fit=crop&w=1950&q=80',
+        'content': `Laborum voluptate consequat exercitation non ipsum sit occaecat pariatur exercitation ea aute enim sit consequat. Sunt velit ea dolor sunt nulla enim consectetur eiusmod eu occaecat. Labore ullamco reprehenderit occaecat mollit voluptate ut enim cillum id qui culpa. Labore fugiat pariatur pariatur proident ut ullamco Lorem ullamco elit. Nisi commodo dolore nisi ullamco labore.
+
+        Cillum id id eu occaecat ex amet proident et amet do aliqua pariatur adipisicing. Sint sint reprehenderit elit pariatur voluptate enim deserunt quis. Reprehenderit sit velit ullamco non mollit in exercitation excepteur. Labore laborum nostrud ullamco incididunt minim aliquip fugiat anim amet amet nulla duis. Commodo consectetur minim sunt ullamco dolore aliquip nostrud culpa laboris ipsum aliqua occaecat sunt. Ullamco ex nostrud laboris ad qui ut. Consequat laboris sint sunt et laborum minim non ea esse dolore dolor ut incididunt duis.
+        
+        Occaecat duis quis incididunt amet est cupidatat eu tempor laborum reprehenderit quis esse sit. Proident tempor sint ea cillum. Consectetur ex dolor ullamco mollit eiusmod irure deserunt dolor. Aliqua sunt pariatur non magna voluptate exercitation elit sunt non eu labore. Nulla occaecat eiusmod irure Lorem quis Lorem amet cillum cillum labore et duis dolor.
+        
+        Culpa in elit veniam fugiat duis aliqua. Minim fugiat id pariatur dolor officia sint adipisicing eiusmod non. Id deserunt aute adipisicing nostrud dolore. Do nostrud minim consequat dolore tempor laboris fugiat Lorem mollit sint culpa amet anim elit. Incididunt cillum ut consectetur dolore ex eu.
+        
+        Eiusmod laborum et consequat enim voluptate fugiat cillum enim irure amet irure. Cupidatat cillum consectetur laborum duis ex commodo. Fugiat ea fugiat deserunt exercitation quis culpa non culpa do voluptate. Veniam dolor in nostrud ad consequat elit id deserunt incididunt ullamco aute minim labore ad. Ex fugiat tempor Lorem ullamco est proident cillum irure.`
+
     },
     {
-        "id": 3,
-        "title": "MeChef.ga",
-        "category": "Progressive Web App",
-        "img": 'http://lorempixel.com/400/400'
+        'id': 3,
+        'date': '12/06/2018',
+        'title': 'MeChef.ga',
+        'category': 'Progressive Web App',
+        'img': 'http://lorempixel.com/400/400',
+        'content': `Laborum voluptate consequat exercitation non ipsum sit occaecat pariatur exercitation ea aute enim sit consequat. Sunt velit ea dolor sunt nulla enim consectetur eiusmod eu occaecat. Labore ullamco reprehenderit occaecat mollit voluptate ut enim cillum id qui culpa. Labore fugiat pariatur pariatur proident ut ullamco Lorem ullamco elit. Nisi commodo dolore nisi ullamco labore.
+
+Cillum id id eu occaecat ex amet proident et amet do aliqua pariatur adipisicing. Sint sint reprehenderit elit pariatur voluptate enim deserunt quis. Reprehenderit sit velit ullamco non mollit in exercitation excepteur. Labore laborum nostrud ullamco incididunt minim aliquip fugiat anim amet amet nulla duis. Commodo consectetur minim sunt ullamco dolore aliquip nostrud culpa laboris ipsum aliqua occaecat sunt. Ullamco ex nostrud laboris ad qui ut. Consequat laboris sint sunt et laborum minim non ea esse dolore dolor ut incididunt duis.
+
+Occaecat duis quis incididunt amet est cupidatat eu tempor laborum reprehenderit quis esse sit. Proident tempor sint ea cillum. Consectetur ex dolor ullamco mollit eiusmod irure deserunt dolor. Aliqua sunt pariatur non magna voluptate exercitation elit sunt non eu labore. Nulla occaecat eiusmod irure Lorem quis Lorem amet cillum cillum labore et duis dolor.
+
+Culpa in elit veniam fugiat duis aliqua. Minim fugiat id pariatur dolor officia sint adipisicing eiusmod non. Id deserunt aute adipisicing nostrud dolore. Do nostrud minim consequat dolore tempor laboris fugiat Lorem mollit sint culpa amet anim elit. Incididunt cillum ut consectetur dolore ex eu.
+
+Eiusmod laborum et consequat enim voluptate fugiat cillum enim irure amet irure. Cupidatat cillum consectetur laborum duis ex commodo. Fugiat ea fugiat deserunt exercitation quis culpa non culpa do voluptate. Veniam dolor in nostrud ad consequat elit id deserunt incididunt ullamco aute minim labore ad. Ex fugiat tempor Lorem ullamco est proident cillum irure.`
+
     },
     {
-        "id": 4,
-        "title": "Cryptocurrency Tracker",
-        "category": "Web App",
-        "img": 'http://lorempixel.com/400/400'
+        'id': 4,
+        'date': '12/06/2018',
+        'title': 'Cryptocurrency Tracker',
+        'category': 'Web App',
+        'img': 'http://lorempixel.com/400/400',
+        'content': `Laborum voluptate consequat exercitation non ipsum sit occaecat pariatur exercitation ea aute enim sit consequat. Sunt velit ea dolor sunt nulla enim consectetur eiusmod eu occaecat. Labore ullamco reprehenderit occaecat mollit voluptate ut enim cillum id qui culpa. Labore fugiat pariatur pariatur proident ut ullamco Lorem ullamco elit. Nisi commodo dolore nisi ullamco labore.
+
+Cillum id id eu occaecat ex amet proident et amet do aliqua pariatur adipisicing. Sint sint reprehenderit elit pariatur voluptate enim deserunt quis. Reprehenderit sit velit ullamco non mollit in exercitation excepteur. Labore laborum nostrud ullamco incididunt minim aliquip fugiat anim amet amet nulla duis. Commodo consectetur minim sunt ullamco dolore aliquip nostrud culpa laboris ipsum aliqua occaecat sunt. Ullamco ex nostrud laboris ad qui ut. Consequat laboris sint sunt et laborum minim non ea esse dolore dolor ut incididunt duis.
+
+Occaecat duis quis incididunt amet est cupidatat eu tempor laborum reprehenderit quis esse sit. Proident tempor sint ea cillum. Consectetur ex dolor ullamco mollit eiusmod irure deserunt dolor. Aliqua sunt pariatur non magna voluptate exercitation elit sunt non eu labore. Nulla occaecat eiusmod irure Lorem quis Lorem amet cillum cillum labore et duis dolor.
+
+Culpa in elit veniam fugiat duis aliqua. Minim fugiat id pariatur dolor officia sint adipisicing eiusmod non. Id deserunt aute adipisicing nostrud dolore. Do nostrud minim consequat dolore tempor laboris fugiat Lorem mollit sint culpa amet anim elit. Incididunt cillum ut consectetur dolore ex eu.
+
+Eiusmod laborum et consequat enim voluptate fugiat cillum enim irure amet irure. Cupidatat cillum consectetur laborum duis ex commodo. Fugiat ea fugiat deserunt exercitation quis culpa non culpa do voluptate. Veniam dolor in nostrud ad consequat elit id deserunt incididunt ullamco aute minim labore ad. Ex fugiat tempor Lorem ullamco est proident cillum irure.`
+
     },
     {
-        "id": 5,
-        "title": "Lady Bugs garden",
-        "category": "Web & Mobile App",
-        "img": 'http://lorempixel.com/400/400'
+        'id': 5,
+        'date': '12/06/2018',
+        'title': 'Lady Bugs garden',
+        'category': 'Web & Mobile App',
+        'img': 'https://images.unsplash.com/photo-1496297679486-1a1607669129?auto=format&fit=crop&w=934&q=80',
+        'content': `Laborum voluptate consequat exercitation non ipsum sit occaecat pariatur exercitation ea aute enim sit consequat. Sunt velit ea dolor sunt nulla enim consectetur eiusmod eu occaecat. Labore ullamco reprehenderit occaecat mollit voluptate ut enim cillum id qui culpa. Labore fugiat pariatur pariatur proident ut ullamco Lorem ullamco elit. Nisi commodo dolore nisi ullamco labore.
+
+        Cillum id id eu occaecat ex amet proident et amet do aliqua pariatur adipisicing. Sint sint reprehenderit elit pariatur voluptate enim deserunt quis. Reprehenderit sit velit ullamco non mollit in exercitation excepteur. Labore laborum nostrud ullamco incididunt minim aliquip fugiat anim amet amet nulla duis. Commodo consectetur minim sunt ullamco dolore aliquip nostrud culpa laboris ipsum aliqua occaecat sunt. Ullamco ex nostrud laboris ad qui ut. Consequat laboris sint sunt et laborum minim non ea esse dolore dolor ut incididunt duis.
+        
+        Occaecat duis quis incididunt amet est cupidatat eu tempor laborum reprehenderit quis esse sit. Proident tempor sint ea cillum. Consectetur ex dolor ullamco mollit eiusmod irure deserunt dolor. Aliqua sunt pariatur non magna voluptate exercitation elit sunt non eu labore. Nulla occaecat eiusmod irure Lorem quis Lorem amet cillum cillum labore et duis dolor.
+        
+        Culpa in elit veniam fugiat duis aliqua. Minim fugiat id pariatur dolor officia sint adipisicing eiusmod non. Id deserunt aute adipisicing nostrud dolore. Do nostrud minim consequat dolore tempor laboris fugiat Lorem mollit sint culpa amet anim elit. Incididunt cillum ut consectetur dolore ex eu.
+        
+        Eiusmod laborum et consequat enim voluptate fugiat cillum enim irure amet irure. Cupidatat cillum consectetur laborum duis ex commodo. Fugiat ea fugiat deserunt exercitation quis culpa non culpa do voluptate. Veniam dolor in nostrud ad consequat elit id deserunt incididunt ullamco aute minim labore ad. Ex fugiat tempor Lorem ullamco est proident cillum irure.`
+
     },
     {
-        "id": 6,
-        "title": "Fireball",
-        "category": "Web & Mobile App",
-        "img": 'http://lorempixel.com/400/400'
+        'id': 6,
+        'date': '12/06/2018',
+        'title': 'Fireball',
+        'category': 'Web & Mobile App',
+        'img': 'http://lorempixel.com/400/400',
+        'content': `Laborum voluptate consequat exercitation non ipsum sit occaecat pariatur exercitation ea aute enim sit consequat. Sunt velit ea dolor sunt nulla enim consectetur eiusmod eu occaecat. Labore ullamco reprehenderit occaecat mollit voluptate ut enim cillum id qui culpa. Labore fugiat pariatur pariatur proident ut ullamco Lorem ullamco elit. Nisi commodo dolore nisi ullamco labore.
+
+Cillum id id eu occaecat ex amet proident et amet do aliqua pariatur adipisicing. Sint sint reprehenderit elit pariatur voluptate enim deserunt quis. Reprehenderit sit velit ullamco non mollit in exercitation excepteur. Labore laborum nostrud ullamco incididunt minim aliquip fugiat anim amet amet nulla duis. Commodo consectetur minim sunt ullamco dolore aliquip nostrud culpa laboris ipsum aliqua occaecat sunt. Ullamco ex nostrud laboris ad qui ut. Consequat laboris sint sunt et laborum minim non ea esse dolore dolor ut incididunt duis.
+
+Occaecat duis quis incididunt amet est cupidatat eu tempor laborum reprehenderit quis esse sit. Proident tempor sint ea cillum. Consectetur ex dolor ullamco mollit eiusmod irure deserunt dolor. Aliqua sunt pariatur non magna voluptate exercitation elit sunt non eu labore. Nulla occaecat eiusmod irure Lorem quis Lorem amet cillum cillum labore et duis dolor.
+
+Culpa in elit veniam fugiat duis aliqua. Minim fugiat id pariatur dolor officia sint adipisicing eiusmod non. Id deserunt aute adipisicing nostrud dolore. Do nostrud minim consequat dolore tempor laboris fugiat Lorem mollit sint culpa amet anim elit. Incididunt cillum ut consectetur dolore ex eu.
+
+Eiusmod laborum et consequat enim voluptate fugiat cillum enim irure amet irure. Cupidatat cillum consectetur laborum duis ex commodo. Fugiat ea fugiat deserunt exercitation quis culpa non culpa do voluptate. Veniam dolor in nostrud ad consequat elit id deserunt incididunt ullamco aute minim labore ad. Ex fugiat tempor Lorem ullamco est proident cillum irure.`
+
     }
 ]
